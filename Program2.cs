71767f4e62a307aeb6d44403b1f1529010a2fd2d
DNK628 <bonnsfag@gmail.com>
2024-12-16 15:42:15 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace Lecture8.Example2
+{
+    class Program
+    {
+        static void FindMinMax<T>(T[] array, out T min, out T max) where T : IComparable<T>
+        {
+            if (array == null || array.Length == 0)
+                throw new ArgumentException("Масив не може бути пустим");
+
+            min = array[0];
+            max = array[0];
+
+            foreach (T item in array)
+            {
+                if (item.CompareTo(min) < 0)
+                    min = item;
+                if (item.CompareTo(max) > 0)
+                    max = item;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.5, 2.3, 0.7, 4.1 };
+
+            FindMinMax(intArray, out int intMin, out int intMax);
+            Console.WriteLine($"int: min={intMin}, max={intMax}");
+
+            FindMinMax(doubleArray, out double doubleMin, out double doubleMax);
+            Console.WriteLine($"double: min={doubleMin}, max={doubleMax}");
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Program3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace Lecture8.Example3
+{
+    class GenericCalculator<T> where T : IComparable<T>
+    {
+        private T value;
+
+        public GenericCalculator(T initialValue)
+        {
+            value = initialValue;
+        }
+
+        public T FindSmaller(T a, T b)
+        {
+            return a.CompareTo(b) < 0 ? a : b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            var calculator = new GenericCalculator<int>(0);
+            Console.WriteLine($"Менше з 5 і 10: {calculator.FindSmaller(5, 10)}");
+
+            var doubleCalculator = new GenericCalculator<double>(0.0);
+            Console.WriteLine($"Менше з 2.5 і 3.5: {doubleCalculator.FindSmaller(2.5, 3.5)}");
+
+            Console.ReadKey();
+        }
+    }
+}
+
